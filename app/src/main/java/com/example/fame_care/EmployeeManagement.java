4453dee85c97d94980986d9f9da3b3da03d7e1ab
ImalshaShavindi99 <imalshashavindi@gmail.com>
2021-09-18 22:45:43 +0530

    commit new

---
+++ app/src/main/java/com/example/fame_care/EmployeeManagement.java
@@ -0,0 +1,14 @@
+package com.example.fame_care;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EmployeeManagement extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_employee_management);
+    }
+}
