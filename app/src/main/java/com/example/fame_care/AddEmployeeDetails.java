d559b2d3fd62c774adab7a60c99091a01bd8b7ca
ImalshaShavindi99 <imalshashavindi@gmail.com>
2021-09-19 00:54:14 +0530

    commit new

---
+++ app/src/main/java/com/example/fame_care/AddEmployeeDetails.java
@@ -0,0 +1,14 @@
+package com.example.fame_care;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddEmployeeDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_employee_details);
+    }
+}
