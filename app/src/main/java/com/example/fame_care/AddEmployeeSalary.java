15ab8aee27bf8a3e9cbd58b9e4204a759d51aa46
ImalshaShavindi99 <imalshashavindi@gmail.com>
2021-09-20 01:10:56 +0530

    commit new1

---
+++ app/src/main/java/com/example/fame_care/AddEmployeeSalary.java
@@ -0,0 +1,14 @@
+package com.example.fame_care;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddEmployeeSalary extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_employee_salary);
+    }
+}
---
+++ app/src/main/java/com/example/fame_care/AddWorkSchedule.java
@@ -0,0 +1,14 @@
+package com.example.fame_care;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddWorkSchedule extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_work_schedule);
+    }
+}
